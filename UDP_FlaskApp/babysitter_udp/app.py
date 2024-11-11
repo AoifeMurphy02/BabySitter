15ff8c87392acc2f9570a438c232a7a72b225e92
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2024-11-11 23:43:23 +0000

    Publish message logic and error handling

--- UDP_FlaskApp/babysitter_udp/app.py
+++ UDP_FlaskApp/babysitter_udp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.exceptions import PubNubException
@@ -13,6 +13,20 @@ pnconfig.subscribe_key = 'sub-c-f8bff00d-78e1-4bb1-86d5-e81d5043ed37'
 pnconfig.publish_key = 'pub-c-a5e694de-d708-4674-9f6a-9df8bdae40a7'
 pnconfig.secret_key = 'sec-c-MzVlZDVmMzUtZjU3OC00YjgzLTg2ZWEtN2NlZWIyNWEwNGQ3'  
 pnconfig.uuid ='Baby-device'
+
+pubnub =PubNub(pnconfig)
+
+# granting access to post to channel
+
+def grant_access():
+    pubnub.grant()\
+        .channels("New-channel")\
+        .read(True)\
+        .write(True)\
+        .manage(True)\
+        .sync()
+
+grant_access()
 
 SPORTS = ["Basketball", "Badminton","Volleyball"]
 REGISTRANTS = {}
@@ -40,6 +54,28 @@ def register():
         return render_template("error.html", message=message)
     REGISTRANTS[name] = sport
     return redirect("/registrants")
+
+# publish message to pubnub
+
+@app.route('/publish', methods=['POST'])
+def publish_message():
+    message = request.json.get('message')
+    if message:
+        try:
+            
+            response = pubnub.publish().channel('New-channel').message({'text': message}).sync()
+
+            # sucess
+            response_data = {
+                'status': 'success' if not response.status.is_error() else 'error',
+                'status_code': response.status.status_code  
+            }
+            
+            return jsonify(response_data)
+        except PubNubException as e:
+            return jsonify({'status': 'error', 'message': str(e)}), 500
+    else:
+        return jsonify({'status': 'error', 'message': 'No message provided'}), 400
 
     # return render_template("success.html")
 if __name__ == "__main__":
