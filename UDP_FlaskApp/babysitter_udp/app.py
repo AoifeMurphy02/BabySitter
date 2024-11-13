7a3a13d6905f06dc26a60f0544e92305a2633237
npulkis <d00239038@student.dkit.ie>
2024-11-13 01:58:13 +0000

    Added inital login and signup functionality with validation

--- UDP_FlaskApp/babysitter_udp/app.py
+++ UDP_FlaskApp/babysitter_udp/app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, session
+from flask_mysqldb import MySQL
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.exceptions import PubNubException
-import os
+from dotenv import load_dotenv
+import os, re ,bcrypt
 
 
 app = flask = Flask(__name__)
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
+
+# MySQL configurations 
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca":  os.path.join(os.path.dirname(__file__), 'eu-west-1-bundle.pem')}} 
+
+mysql = MySQL(app)
+
 
 # pubnub keys and ID
 pnconfig = PNConfiguration()
@@ -35,6 +49,69 @@ REGISTRANTS = {}
 def index():
     return render_template("index.html", sports=SPORTS)
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Password requirements check
+        if not re.fullmatch(r'^(?=.*[A-Z])(?=.*\d).{8,}$', password):
+            flash('Password must be at least 8 characters long, contain at least one digit, and one uppercase letter.', 'danger')
+            return render_template('signup.html')
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        # Check if username already exists
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        existing_user = cur.fetchone()
+        if existing_user:
+            flash('Username already taken. Please choose a different username.', 'danger')
+            return render_template('signup.html')
+        
+        # Save user to database
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(username, password) VALUES(%s, %s)", (username, hashed_password))
+        mysql.connection.commit()
+        cur.close()
+        
+        flash('You have successfully signed up!', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Fetch user from database
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username=%s", [username])
+        user = cur.fetchone()
+        cur.close()
+        
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[2].encode('utf-8')):
+            session['username'] = user[1]
+            flash('Login successful!', 'success')
+            return redirect(url_for('loggedin'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html')
+
+
+@app.route('/loggedin')
+def loggedin():
+    if 'username' in session:
+        return f'Welcome {session["username"]}!'
+    else:
+        return redirect(url_for('login'))
+    
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
 
 @app.route("/registrants")
 def registrants():
