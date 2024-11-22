8956fde90811c0235e244c86074f8fa090e5dc2f
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2024-11-22 12:50:52 +0000

    fixed the video and sound now working with pubnub

--- UDP_FlaskApp/babysitter_udp/my_db.py
+++ UDP_FlaskApp/babysitter_udp/my_db.py
@@ -25,3 +25,26 @@ def add_babysitter(user_name, password, name, email):
 
 def get_babysitter_by_email(email):
         return BabySitterLogin.query.filter_by(email=email).first()
+
+class BabySitterData():
+       __tablename__ = 'baby_data'
+       id = db.Column(db.Integer, primary_key=True, nullable=False)
+       time = db.Column(db.TIMESTAMP, nullable=False, server_default=db.func.current_timestamp())
+       sound_pitch = db.Column(db.Float,nullable=True)
+       temperature = db.Column(db.Float,nullable=True)
+       humidity = db.Column(db.Float,nullable=True)
+       camera_feed_path= db.Column(db.Float,nullable=True)
+       audio= db.Column(db.Float,nullable=True)
+
+       def __init__(self, sound_pitch, temperature, humidity, camera_feed_path, audio ):
+        self.sound_pitch = sound_pitch
+        self.temperature = temperature
+        self.humidity = humidity
+        self.camera_feed_path = camera_feed_path
+        self.audio = audio
+
+
+def add_baby_data(sound_pitch, temperature, humidity, camera_feed_path, audio):
+        new_baby_data =  BabySitterData(sound_pitch = sound_pitch, temperature = temperature, humidity = humidity, camera_feed_path =camera_feed_path, audio = audio)
+        db.session.add(new_baby_data)
+        db.session.commit()
